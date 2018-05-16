e8e82c56c1ecc524e8d50330157fb7f418e51fb6
wangpeng <15380790458@163.com>
2018-05-16 21:04:15 +0800

    初次提交

---
+++ src/main/java/com/ben/cloak/CloakApplication.java
@@ -0,0 +1,12 @@
+package com.ben.cloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloakApplication.class, args);
+	}
+}
